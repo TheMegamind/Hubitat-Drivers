54cfeefd9178edaaadb8e9589cb17dddc95dea68
TheMegamind <mail@throckmorton.us>
2022-12-24 10:05:31 -0800

    Create replicaTempHumiditysensor.groovy

---
+++ replicaTempHumiditysensor.groovy
@@ -0,0 +1,100 @@
+/**
+*  Copyright 2022 bthrock
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*/
+@SuppressWarnings('unused')
+public static String version() {return "1.2.0"}
+
+metadata 
+{
+    definition(name: "Replica Temp/Humidity Sensor", namespace: "replica", author: "bthrock", importUrl:"https://raw.githubusercontent.com/bthrock/Hubitat/main/hubiThingsReplica/devices/replicaMultipurposeSensor.groovy")
+    {
+        capability "Battery"
+        capability "Configuration"
+        capability "Relative Humidity Measurement"
+        capability "TemperatureMeasurement"
+        
+        attribute "healthStatus", "enum", ["offline", "online"]
+    }
+    preferences {   
+    }
+}
+
+def installed() {
+	initialize()
+}
+
+def updated() {
+	initialize()    
+}
+
+def initialize() {
+    updateDataValue("triggers", groovy.json.JsonOutput.toJson(getReplicaTriggers()))
+    updateDataValue("commands", groovy.json.JsonOutput.toJson(getReplicaCommands()))
+}
+
+def configure() {
+    log.info "${device.displayName} configured default rules"
+    initialize()
+    updateDataValue("rules", getReplicaRules())
+    sendCommand("configure")
+}
+
+// Methods documented here will show up in the Replica Command Configuration. These should be mostly setter in nature. 
+Map getReplicaCommands() {
+    return ([ "setBatteryValue":[[name:"battery*",type:"NUMBER"]], 
+              "setHumidityValue":[[name:"humidity*",type:"NUMBER"]],
+              "setTemperatureValue":[[name:"temperature*",type:"NUMBER"]]])
+}
+
+def setBatteryValue(value) {
+    String descriptionText = "${device.displayName} battery level is $value %"
+    sendEvent(name: "battery", value: value, unit: "%", descriptionText: descriptionText)
+    log.info descriptionText
+}
+
+def setHumidityValue(value) {
+    String descriptionText = "${device.displayName} relative humidity is $value %"
+    sendEvent(name: "humidity", value: value, unit: "%", descriptionText: descriptionText)
+    log.info descriptionText
+}
+
+def setTemperatureValue(value) {
+    String unit = "°${getTemperatureScale()}"
+    String descriptionText = "${device.displayName} temperature is $value $unit"
+    sendEvent(name: "temperature", value: value, unit: unit, descriptionText: descriptionText)
+    log.info descriptionText
+}
+
+// Methods documented here will show up in the Replica Trigger Configuration. These should be all of the native capability commands
+Map getReplicaTriggers() {
+    return (["refresh":[]])
+}
+
+private def sendCommand(String name, def value=null, String unit=null, data=[:]) {
+    parent?.deviceTriggerHandler(device, [name:name, value:value, unit:unit, data:data, now:now])
+}
+
+void refresh() {
+    sendCommand("refresh")
+}
+
+String getReplicaRules() {
+    return """{"version":1,"components":[ 
+        
+        {"trigger":{"title":"IntegerPercent","type":"attribute","properties":{"value":{"type":"integer","minimum":0,"maximum":100},"unit":{"type":"string","enum":["%"],"default":"%"}},"additionalProperties":false,"required":["value"],"capability":"battery","attribute":"battery","label":"attribute: battery.*"},"command":{"name":"setBatteryValue","label":"command: setBatteryValue(battery*)","type":"command","parameters":[{"name":"battery*","type":"NUMBER"}]},"type":"smartTrigger","mute":true},
+
+        {"trigger":{"title":"IntegerPercent","type":"attribute","properties":{"value":{"type":"integer","minimum":0,"maximum":100},"unit":{"type":"string","enum":["%"],"default":"%"}},"additionalProperties":false,"required":["value"],"capability":"humidity","attribute":"humidity","label":"attribute: humidity.*"},"command":{"name":"setHumidityValue","label":"command: setHumidityValue(humidity*)","type":"command","parameters":[{"name":"humidity*","type":"NUMBER"}]},"type":"smartTrigger","mute":true},
+        
+        {"trigger":{"type":"attribute","properties":{"value":{"title":"TemperatureValue","type":"number","minimum":-460,"maximum":10000},"unit":{"type":"string","enum":["F","C"]}},"additionalProperties":false,"required":["value","unit"],"capability":"temperatureMeasurement","attribute":"temperature","label":"attribute: temperature.*"},"command":{"name":"setTemperatureValue","label":"command: setTemperatureValue(temperature*)","type":"command","parameters":[{"name":"temperature*","type":"NUMBER"}]},"type":"smartTrigger","mute":true}]}"""
+        
+}
