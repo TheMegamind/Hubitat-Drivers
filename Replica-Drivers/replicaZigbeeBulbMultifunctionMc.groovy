88b602c3a554b45c4182b705a4f89f5375c1175c
Megamind <mail@throckmorton.us>
2025-03-13 16:45:04 -0700

    Release 1.3.1

--- Replica-Drivers/replicaZigbeeBulbMultifunctionMc.groovy
+++ Replica-Drivers/replicaZigbeeBulbMultifunctionMc.groovy
@@ -1,6 +1,6 @@
 /**
 *  Original Driver Copyright 2023 David Gutheinz
-*  Modified to include custom capabilites & commands in MarianoColmenarejo'sZigbee Bulb Multifunction Mc by Bob Throckmorton
+*  Modified to include custom capabilites & commands in MarianoColmenarejo's Zigbee Bulb Multifunction Mc by Bob Throckmorton
 *
 *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 *  in compliance with the License. You may obtain a copy of the License at:
